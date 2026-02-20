e3b0419c25b91bd61677be6f118e1966e950ed31
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2026-02-20 12:30:55 +0100

    deleting useless things & installing packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-slate-950 text-slate-100 flex items-center justify-center">
+      <div className="flex items-center justify-center min-h-screen bg-slate-950 text-slate-100">
         <h1 className="text-4xl font-bold tracking-tight">Champagne League</h1>
       </div>
     </>
