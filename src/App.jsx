4399f89ae17a0d2940d1200a2f861c3324bdce5a
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2026-02-20 01:07:04 +0100

    website setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-slate-950 text-slate-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold tracking-tight">Champagne League</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
