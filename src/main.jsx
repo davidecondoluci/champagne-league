58516477cb9b7cd707aff2f1f56d38b5ec4cd012
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2026-03-09 02:27:41 +0100

    lenis scroll

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import App from "./App.jsx";
-import Lenis from "lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { lenis } from "./lenis.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const lenis = new Lenis();
 lenis.on("scroll", ScrollTrigger.update);
 gsap.ticker.add((time) => {
   lenis.raf(time * 1000);
